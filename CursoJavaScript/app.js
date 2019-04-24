4db558245260031f57d53171b7e2abee76b6ffa5
NahumFG <ngutierrez2507@gmail.com>
2019-04-24 14:36:48 -0500

    Video 10

---
+++ CursoJavaScript/app.js
@@ -0,0 +1,22 @@
+//document.getElementById('app').innerHTML = "Hola Mundo";
+/*
+let nombre = prompt('cual es tu nombre?');
+let edad = prompt('cual es tu edad?');
+document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad} años`
+*/
+/*
+console.log("Enviando a la consola");
+console.log('Hasta aqui todo bien');
+console.log(true);
+console.log('ID:' + 20);
+*/
+//var let const --var es antiguo
+
+
+//var nombre = 'Juan';
+//var nombre = 'Chorri';
+//nombre = 'Jhon';
+let nombre = 'Juan';
+let nombre = 'Leo'; //Let no permite redeclarar, pero cambia mediante la ejecucion
+
+console.log(nombre);
