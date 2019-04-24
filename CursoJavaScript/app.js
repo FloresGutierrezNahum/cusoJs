21664c56d2823530e53cadf3b6f76ab673cba9e5
NahumFG <ngutierrez2507@gmail.com>
2019-04-24 15:00:51 -0500

    Manejo de Strings

--- CursoJavaScript/app.js
+++ CursoJavaScript/app.js
@@ -10,13 +10,43 @@ console.log('Hasta aqui todo bien');
 console.log(true);
 console.log('ID:' + 20);
 */
+
+// *********Strings
 //var let const --var es antiguo
 
+/*
 
 //var nombre = 'Juan';
 //var nombre = 'Chorri';
 //nombre = 'Jhon';
-let nombre = 'Juan';
-let nombre = 'Leo'; //Let no permite redeclarar, pero cambia mediante la ejecucion
 
+let nombre = 'Juan';
+//let nombre = 'Leo'; //Let no permite redeclarar, pero cambia mediante la ejecucion
+nombre = 'y lala "a"';
+nombre = 'lalala \'aaaa\' ';
+//const : son constantes q no camniaran de valor
+
+//Concatenar texto --> Forma antigua
+nombre = 'mensaje1' + "Mensaje 2";
 console.log(nombre);
+
+let aprendiendo = 'Aprendiendo JavaScript, CSS, HTML para ser Frontend',
+    tecnologia = 'Js';
+
+//Concatenar --> JsModerno
+console.log(`${aprendiendo} ${tecnologia}`);
+console.log(aprendiendo.length);
+console.log(tecnologia.concat(' ', 'Es genial')); //Antigua
+console.log(tecnologia.toUpperCase());
+
+console.log(aprendiendo.indexOf('CSS'));
+console.log(aprendiendo.slice(-1));
+console.log(aprendiendo.slice(0, 11));
+console.log(aprendiendo.split(' '));
+console.log(aprendiendo.split(', '));
+console.log(aprendiendo.replace('CSS', 'PHP'));
+console.log(aprendiendo.includes('CSS'));
+console.log(tecnologia.repeat(10));
+
+
+*/
